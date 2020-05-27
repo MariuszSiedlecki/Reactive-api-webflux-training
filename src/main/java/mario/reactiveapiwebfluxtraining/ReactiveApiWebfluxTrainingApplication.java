bac32db935cc5e23ddfb610cd4100b64fecd8e6c
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-05-27 10:44:48 +0200

    Initial commit

---
+++ src/main/java/mario/reactiveapiwebfluxtraining/ReactiveApiWebfluxTrainingApplication.java
@@ -0,0 +1,13 @@
+package mario.reactiveapiwebfluxtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiWebfluxTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApiWebfluxTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mario/reactiveapiwebfluxtraining/ReactiveApiWebfluxTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package mario.reactiveapiwebfluxtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiWebfluxTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
