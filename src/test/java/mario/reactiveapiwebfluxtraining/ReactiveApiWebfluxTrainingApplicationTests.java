7d445ee4eb1efcc1d4f86280e4084f0fd541f06f
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-05-29 01:31:01 +0200

    Test

--- src/test/java/mario/reactiveapiwebfluxtraining/ReactiveApiWebfluxTrainingApplicationTests.java
+++ src/test/java/mario/reactiveapiwebfluxtraining/ReactiveApiWebfluxTrainingApplicationTests.java
@@ -1,13 +1,26 @@
 package mario.reactiveapiwebfluxtraining;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactiveApiWebfluxTrainingApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Flux.just("Pankracy", "Gerwazy", "Piotr", "Ania")
+                .map(String::toLowerCase)
+                .subscribe(element -> logger.info(element));
+
+        Mono.just("Leon")
+                .map(String::toUpperCase)
+                .subscribe(element -> logger.info(element));
     }
+
 
 }
